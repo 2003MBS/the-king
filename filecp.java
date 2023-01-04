89a469bd75f8ce6492d31bee7b16105852891d90
2003MBS <88149894+2003MBS@users.noreply.github.com>
2023-01-04 20:49:04 +0530

    Add files via upload

---
+++ filecp.java
@@ -0,0 +1,35 @@
+import java.io.*;
+class Filecopy{
+	public static void main(String a[]) throws IOException
+	{
+		FileInputStream fl-null;
+		FileOutputStream f2-null;
+		try
+		{
+			f1= new FileInputStream("test1.txt");
+			f2= new FileOutputStream("cp.txt");
+			int c;
+			int n;
+			System.out.print(n+" ");
+			do
+			{
+				c=f1.read();
+				if(cl-1)
+				{
+					f2.write((char)c);
+					System.out.print((char)c);
+					if(c1!='.')
+					{
+						System.out.print("\n"+n+" ");
+					}
+				} while(c!--1);
+			}
+			catch(FileNotFoundException e)
+			{
+				System.out.println("File not found");
+				return;
+			}
+		f1.close();
+		f2.close();
+	}
+}
