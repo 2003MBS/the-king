1ec4574900390eb39110d4502317c0092f57ebe2
2003MBS <88149894+2003MBS@users.noreply.github.com>
2023-01-05 08:32:37 +0530

    Update multithread.java

--- multithread.java
+++ multithread.java
@@ -1,41 +1,50 @@
-import java.util.Random
-class Randon Thread extends Thread{
-	public void run(){
-		Random r = new Random();
-		for(int i=0;i<20;i++){
-			int n = r.nextInt(100);
-			if(n%2==0)
-			{
-				new Even(n).start();
-			}
-			else
-			{
-				new Odd(n).start();
-			}
-		}
-	}
+import java.util.*;
+class odd extends Thread
+{
+    private int num1=0;
+    public void run()
+    {
+        for(int i=0;i<=num1;i++)
+        {
+            if(num1%2==0)
+            {
+                System.out.print(num1+" ");
+            }
+            num1=num1+1;
+        }
+    }
 }
-class Even extends Thread {
-	private int num;
-	public Even(int num){
-		this.num = num;
-	}
-	public void run(){
-		System.out.println("Square of "+num+"="+num"num);
-	}
+class even extends Thread
+{
+    private int num2=0;
+    public void run()
+    {
+        for(int i=0;i<=num2;i++)
+        {
+            if(num1%2==0)
+            {
+                System.out.print(num2+" ");
+            }
+            num2=num2+1;
+        }
+    }
 }
-class Odd extends Thread{
-	private int num;
-	public Odd(int num){ 
-		this.num = num;
-	}
-	public void run(){ 
-		System.out.println("Cube of "+num+": num*num*num);
-	}
-}
-class Test{
-	public static void main(String args[]){
-		Randon Thread r = new Randon Thread():
-		r.start():
-	}
-}
+class test
+{
+    public static void main(String args[])
+    {
+        odd o = new odd();
+        even e = new even();
+        try
+        {
+            System.out.println("Odd numbers");
+            o.start();
+            o.join();
+        }
+        catch(Exception e)
+        {
+            System.out.println("exception occured");
+        }
+        System.out.println("\nEven nunbers");
+        e.start();
+    }
